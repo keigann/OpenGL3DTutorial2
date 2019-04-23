80655b97c419630325854f11e240efe8c28f494f
奎成  山崎 <shanqikuicheng@gmail.com>
2019-04-23 16:59:21 +0900

    プログラム記述１

--- Scene.h
+++ Scene.h
@@ -1,1 +1,41 @@
-#pragma once
+/*
+*@file scene.h
+*/
+#ifndef SCENE_H_INCLUDED
+#define SCENE_H_INCLUDED
+#include <memory>
+#include <string>
+
+class SceneStack;
+/*
+*シーンの基底クラス
+*/
+
+class Scene
+{
+public:
+	Scene(const char* name);
+	Scene(const Scene&) = delete;
+	Scene& operator=(const Scene&) = delete;
+	virtual ~Scene();
+
+	virtual bool Initialize() = 0 {}
+	virtual void ProcessIupot() = 0 {}
+	virtual void Updeta(float) = 0 {}
+	virtual void Render() = 0 {}
+	virtual void Finalize() = 0 {}
+	
+	virtual void Play();
+	virtual void Stop();
+	virtual void Show();
+	virtual void Hide();
+
+private:
+	std::string name;
+	bool isVisible = true;
+	bool isActive = true;
+};
+using ScenePtr = std::shared_ptr<Scene>;
+
+#endif  //SCENE_H_INCLUDED
+
